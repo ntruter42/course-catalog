51b411edff1caf69d3e7213666ef197455dec874
ntruter42 <ntruter42@gmail.com>
2023-04-20 13:40:49 +0200

    Update index.html, styles.css, simple-grid.css, Add course-details.js - added testimonials section with sample image and sample text

---
+++ responsive-page/js/course-details.js
@@ -0,0 +1,12 @@
+// Create variable for all course elements
+// Get each element using a for loop
+// Add an event listener for each course item button
+// 
+
+
+const courseElements = document.querySelectorAll(".course-item");
+
+for (let courseItem of courseElements) {
+	const detailsButton = courseItem.querySelector(":scope > button");
+	// Work in progress
+}
