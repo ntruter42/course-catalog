c945e92fa6322f0b8317be4cc084f42c93c03a15
ntruter42 <ntruter42@gmail.com>
2023-04-20 15:02:03 +0200

    Update test/cost.test.js - fixed first functional test expected test result

--- course-cost/test/cost.test.js
+++ course-cost/test/cost.test.js
@@ -1,14 +1,30 @@
 describe('courseCost', function () {
 	it('Courses should get different prices across discount window', function () {
-		const result = {
+		assert.deepEqual({
 			"status": "success",
 			"code": "r1",
-			"description": "",
-			"cost": "",
-			"discount": "",
-			"amountDue": ""
-		};
+			"description": "Ruby on Rails intro",
+			"cost": 5675.00,
+			"discount": (5675 / 10) * 4,
+			"amountDue": (5675 / 10) * 6
+		}, courseCost("r1", "2023-05-21"));
 
-		assert.deepEqual(result, courseCost("r1", "2023-05-21"));
-	});
+		assert.deepEqual({
+			"status": "success",
+			"code": "r1",
+			"description": "Ruby on Rails intro",
+			"cost": 5675.00,
+			"discount": (5675 / 10) * 4,
+			"amountDue": (5675 / 10) * 6
+		}, courseCost("r1", "2023-05-21"));
+
+		assert.deepEqual({
+			"status": "success",
+			"code": "r1",
+			"description": "Ruby on Rails intro",
+			"cost": 5675.00,
+			"discount": (5675 / 10) * 4,
+			"amountDue": (5675 / 10) * 6
+		}, courseCost("r1", "2023-05-21"));
+});
 });
